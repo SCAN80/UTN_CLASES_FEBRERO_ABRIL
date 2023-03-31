ff8c2f08342b87fc91a2945631204df11d2f9a0e
lolazo455 <mati.a.gimenez2002@gmail.com>
2023-03-30 21:00:50 -0300

    Clase 11

---
+++ DW-c181-turno-noche/clase-11/script.js
@@ -0,0 +1,18 @@
+const formularioUsuarios = document.querySelector(".formUsuarios")
+const usuariosContainerHTML = document.querySelector("#usuariosContainer")
+/* document.getElementsByClassName("inputContainer") / document.querySelectorAll(".inputContainer") */
+
+const usuarios = []
+
+
+formularioUsuarios.addEventListener("submit", (event) =>{
+    event.preventDefault()
+    console.log(formularioUsuarios.provincia.value)
+    usuarios.push({
+        nombre: formularioUsuarios.nombre.value,
+        email: formularioUsuarios.email.value,
+        contrasena: formularioUsuarios.contrasena.value,
+        provincia:formularioUsuarios.provincia.value
+    })
+})
+
